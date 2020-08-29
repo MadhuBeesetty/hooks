86132f277b1c6ad1822bf8a9b252bdf5fd1ff86b
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2020-08-29 01:00:06 -0700

    React hooks useState with counter example

---
+++ hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClassCountr from './Components/ClassCounter';
+import HookCountr from './Components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassCountr />
+      <HookCountr />
+    </div>
+  );
+}
+
+export default App;
